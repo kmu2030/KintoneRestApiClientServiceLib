6aa2fec1bbaa07723dc0e66d3dbca82c1c1af5ca
kmu <kmu.cumin@gmail.com>
2025-06-07 15:40:06 +0900

    Update dependent libraries.

--- monitor-local-stunit.sh
+++ monitor-local-stunit.sh
@@ -3,6 +3,7 @@
 prev=$(date +%s)
 clear_threshold=300
 ok_sign='\u2714'
+ignores=('print')
 
 inotifywait -q -m \
 	-e modify \
@@ -17,6 +18,17 @@ while read -r dir file; do
 		clear;
 	fi
 	prev=$now;
+
+	is_ignore=false
+	for ignore in "${ignores[@]}"; do
+		if [[ "$ignore" == "$file" ]]; then
+			is_ignore=true
+			break
+		fi
+	done
+	if "$is_ignore"; then
+		continue
+	fi
 
 	echo -e "$(date -d "@${now}" '+%H:%M:%S') $file";
 	if [ -z "$(tail -1 "$dir\\$file" | grep 'Test suite has no failures.$')" ]; then
